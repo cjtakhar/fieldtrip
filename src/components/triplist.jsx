8925317f360e644a7fccae8d048940cc1fc74917
cjtakhar <cjtakhar@gmail.com>
2023-05-26 23:54:36 -0700

    update

--- src/components/triplist.jsx
+++ src/components/triplist.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { BiHotel } from "react-icons/bi";
 import { AiOutlineCheck } from "react-icons/ai";
 import { FiDelete } from "react-icons/fi";
+import { AiOutlineDollar } from "react-icons/ai";
+import { BsHouseAdd } from "react-icons/bs";
 import "./styles/triplist.css";
 
 const TripList = () => {
   const [trips, setTrips] = useState([]);
   const [newTrip, setNewTrip] = useState("");
   const [selectedTrip, setSelectedTrip] = useState(null);
-  const [selectedHotelOption, setSelectedHotelOption] = useState(null);
+  const [selectedCost, setSelectedCost] = useState("");
+  const [costInputVisible, setCostInputVisible] = useState(false);
 
   useEffect(() => {
     // Retrieve trips from localStorage
@@ -107,119 +110,161 @@ const TripList = () => {
     return id === selectedTrip;
   };
 
-  return (
-    <div className="container-fluid">
-      <div className="trip-container">
-        <h1 className="trip-title">Where to?</h1>
-        <form className="trip-form" onSubmit={handleSubmit}>
-          <label>
-            <input
-              className="trip-input"
-              type="text"
-              name="trip"
-              placeholder="Name a place."
-              autoComplete="off"
-              value={newTrip}
-              onChange={(event) => setNewTrip(event.target.value)}
-            />
-          </label>
-          <input className="trip-btn" type="submit" value="Submit" />
-        </form>
-      </div>
-      {trips.length > 0 && (
-        <div className="trip-list">
-          <h1 className="trip-list-title">LFG.</h1>
-          <ul className="trip-list-ul">
-            {trips.map((trip) => (
-              <li
-                className={`trip-list-li ${
-                  isTripSelected(trip.id) ? "selected" : ""
-                }`}
-                key={trip.id}
-                onClick={() => handleTripClick(trip.id)}
-              >
-                {isTripSelected(trip.id) ? (
-                  <>
-                    <span className="trip-name">
-                      {trip.name}
-                    </span>
-                    <div className="hotel-options">
-                      {trip.hotelOptions.map((option) => (
-                        <form
-                          key={option.id}
-                          className="hotel-form"
-                          onSubmit={(event) =>
-                            handleHotelOptionSubmit(event, trip.id)
-                          }
-                        >
-                          <input
-                            className="hotel-input"
-                            type="text"
-                            name="hotel"
-                            placeholder="Enter hotel website"
-                            value={option.hotelWebsite}
-                            onChange={(event) =>
-                              handleHotelOptionChange(event, trip.id, option.id)
-                            }
-                          />
-                          <button className="hotel-btn" type="submit">
-                            <AiOutlineCheck />
-                          </button>
-                          <button
-                            className="delete-btn"
-                            onClick={() =>
-                              handleDeleteHotelOption(trip.id, option.id)
-                            }
-                          >
-                            <FiDelete />
-                          </button>
-                        </form>
-                      ))}
-                      <button
-                        className="add-hotel-btn"
-                        onClick={(event) =>
+// ...
+
+return (
+  <div className="container-fluid">
+    <div className="trip-container">
+      <h1 className="trip-title">Where to?</h1>
+      <form className="trip-form" onSubmit={handleSubmit}>
+        <label>
+          <input
+            className="trip-input"
+            type="text"
+            name="trip"
+            placeholder="Name a place."
+            autoComplete="off"
+            value={newTrip}
+            onChange={(event) => setNewTrip(event.target.value)}
+          />
+        </label>
+        <input className="trip-btn" type="submit" value="List it" />
+      </form>
+    </div>
+    {trips.length > 0 && (
+      <div className="trip-list">
+        <h1 className="trip-list-title">LFG.</h1>
+        <ul className="trip-list-ul">
+          {trips.map((trip) => (
+            <li
+              className={`trip-list-li ${
+                isTripSelected(trip.id) ? "selected" : ""
+              }`}
+              key={trip.id}
+              onClick={() => handleTripClick(trip.id)}
+            >
+              {isTripSelected(trip.id) ? (
+                <>
+                  <span className="trip-name">{trip.name}</span>
+                  <div className="hotel-options">
+                    {trip.hotelOptions.map((option) => (
+                      <form
+                        key={option.id}
+                        className="hotel-form"
+                        onSubmit={(event) =>
                           handleHotelOptionSubmit(event, trip.id)
                         }
                       >
-                        Add Hotel Option
-                      </button>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <span className="trip-name">
-                      {trip.name}
-                      {trip.hotelOptions.length > 0 && (
-                        <a
-                          className="hotel-icon-link"
-                          href={trip.hotelOptions[0].hotelWebsite}
-                          target="_blank"
-                          rel="noopener noreferrer"
+                        <input
+                          className="hotel-input"
+                          type="text"
+                          name="hotel"
+                          placeholder="Enter hotel website"
+                          value={option.hotelWebsite}
+                          onChange={(event) =>
+                            handleHotelOptionChange(
+                              event,
+                              trip.id,
+                              option.id
+                            )
+                          }
+                        />
+                        <button className="hotel-btn" type="submit">
+                          <AiOutlineCheck />
+                        </button>
+                        <button
+                          className="delete-btn"
+                          onClick={() =>
+                            handleDeleteHotelOption(trip.id, option.id)
+                          }
                         >
-                          <BiHotel className="hotel-icon" />
-                        </a>
-                      )}
-                    </span>
-                  </>
-                )}
-                {isTripSelected(trip.id) && (
-                  <div className="trip-options">
+                          <FiDelete />
+                        </button>
+                      </form>
+                    ))}
                     <button
-                      className="delete-btn"
-                      onClick={() => handleDeleteTrip(trip.id)}
+                      className="add-hotel-btn"
+                      onClick={(event) =>
+                        handleHotelOptionSubmit(event, trip.id)
+                      }
                     >
-                      <FiDelete />
+                      <BsHouseAdd />
+                    </button>
+                    <button
+                      className={`cost-btn ${
+                        isTripSelected(trip.id) && costInputVisible
+                          ? "active"
+                          : ""
+                      }`}
+                      onClick={() => {
+                        setSelectedTrip(trip.id);
+                        setCostInputVisible(true);
+                      }}
+                    >
+                      <span className="cost-btn-icon">
+                        <AiOutlineDollar />
+                      </span>
+                      {isTripSelected(trip.id) && costInputVisible ? (
+                        <input
+                          className="cost-input"
+                          type="text"
+                          placeholder="cost"
+                          value={selectedCost}
+                          onChange={(event) =>
+                            setSelectedCost(event.target.value)
+                          }
+                          onBlur={() => setCostInputVisible(false)}
+                          autoFocus
+                        />
+                      ) : (
+                        <span className="cost-value">{selectedCost}</span>
+                      )}
                     </button>
                   </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
-  
+                </>
+              ) : (
+                <>
+                  <span className="trip-name">
+                    {trip.name}
+                    {trip.hotelOptions.length > 0 && (
+                      <a
+                        className="hotel-icon-link"
+                        href={trip.hotelOptions[0].hotelWebsite}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <BiHotel className="hotel-icon" />
+                      </a>
+                    )}
+                    {trip.hotelOptions.length > 0 && (
+                      <span className="cost-value">
+                          <AiOutlineDollar className="cost-icon" />
+                        {selectedCost}
+                      
+                      </span>
+                    )}
+                  </span>
+                </>
+              )}
+              {isTripSelected(trip.id) && (
+                <div className="trip-options">
+                  <button
+                    className="delete-btn"
+                    onClick={() => handleDeleteTrip(trip.id)}
+                  >
+                    delete trip
+                    <FiDelete />
+                  </button>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
+  </div>
+);
+
 };
 
 export default TripList;
